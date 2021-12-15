ccf1c2ce06d3a4ce8c1ff20c930c4701e660a8be
xianghaibo <xiang.haibo@qq.com>
2021-12-15 22:28:33 +0800

    常见的一些写法

---
+++ Demo2.ts
@@ -0,0 +1,14 @@
+//对象类型
+const xiaojiejie: {
+    name: string,
+    age: number
+} = {
+    name: '花园宝宝',
+    age: 20
+}
+//数组类型
+const xiaojiejies: string[] = ['巴啦啦小魔仙', '魔仙女王', '有了丸子']
+
+// 类
+class Person { };
+const xiedajiao: Person = new Person
